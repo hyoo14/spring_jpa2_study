095c3ed02c1af6047f71f58b8cb2ab963959c3c0
hyunwoo_yoo <hyunwoo_yoo@tmax.co.kr>
2021-06-25 21:03:49 +0900

    feat: section 4 complete(20)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -150,7 +150,7 @@ public class OrderApiController {
             itemName = orderItem.getItem().getName();
             orderPrice = orderItem.getOrderPrice();
             count = orderItem.getCount();
-        }
+        }//
 
 
 
